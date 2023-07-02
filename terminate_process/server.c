18aa9daaf2d29ea51d8ba914b43f30302dff6013
Shintaro Saeki <shisaeki@c1r24s7.42tokyo.jp>
2023-07-02 12:31:50 +0900

    ADD: terminate_process

--- terminate_process/server.c
+++ terminate_process/server.c
@@ -6,22 +6,37 @@
 /*   By: shisaeki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 11:47:39 by shisaeki          #+#    #+#             */
-/*   Updated: 2023/07/01 12:12:53 by shisaeki         ###   ########.fr       */
+/*   Updated: 2023/07/02 12:29:39 by shisaeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 
-int main()
+void	signal_handler(int	signum)
 {
-	pid_t	pid;
-	char buffer[255];
+	char	*message = "Exit server Process";
+	write(STDOUT_FILENO, message, strlen(message));
+	exit(0) ;
+}
+
+int	main()
+{
+	pid_t				pid;
+	struct sigaction	sa;
+	char				buffer[255];
 
 	pid = getpid();
 	sprintf(buffer, "Process ID: %d\n", (int) pid);
 	write(STDOUT_FILENO, buffer, strlen(buffer));
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = signal_handler;
+	sa.sa_flags = 0;
+	sigaction(SIGINT, &sa, NULL);
 
 	while(1)
 	{
